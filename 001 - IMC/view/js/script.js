6a2f402fd7b0454ba980726dcd97f91a2a018159
abdielbatista <abdielb@yahoo.com>
2022-04-25 21:28:06 -0300

    MVC pattern implementation

---
+++ 001 - IMC/view/js/script.js
@@ -0,0 +1,8 @@
+export const elements = {
+    nome: document.getElementById('nome').value,
+    altura: document.getElementById('altura').value,
+    peso: document.getElementById('peso').value,
+    resultado: document.getElementById('resultado'),
+
+    calcular: document.getElementById('calcular')
+};
