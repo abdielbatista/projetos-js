1af04eae9b2b64da5512c69682045c4bf4fd4b84
abdielbatista <abdielb@yahoo.com>
2022-04-26 18:41:27 -0300

    update

--- 001 - IMC/view/js/script.js
+++
@@ -1,8 +0,0 @@
-export const elements = {
-    nome: document.getElementById('nome').value,
-    altura: document.getElementById('altura').value,
-    peso: document.getElementById('peso').value,
-    resultado: document.getElementById('resultado'),
-
-    calcular: document.getElementById('calcular')
-};
